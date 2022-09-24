50c8b261dde036ebaf82b3267ff5fb3a7524879b
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-24 11:51:27 -0700

    Update pytorch_tutorial.py

--- pytorch_tutorial.py
+++ pytorch_tutorial.py
@@ -6,3 +6,9 @@ y = torch.rand(2, 2)
 # every function that has an underscore will do an in-place operation
 y.add_(x)
 print(y)
+
+x = torch.rand(5, 3)
+
+# This prints for all of the rows, but only the first column (index 0)
+print(x[:, 0])
+
